5220fe83970728a90e78ad30badfd4d9d3270356
leo.perrin <perrin.leo@gmail.com>
2025-02-27 17:47:49 +0100

    cleaned up the code generating the input of waybar

--- waybar_daemon.py
+++ waybar_daemon.py
@@ -2,13 +2,38 @@
 
 from etabli import *
 
-def print_current_workspace(SWAY, e):
-    MAIN_ETABLI.get_state_from_wm()
-    print(MAIN_ETABLI)
 
-    
-def format_main_level(lev):
-    return "<span color='0xC00000'>" + lev + "</span>"
+# !TODO! use similar style for all the workspaces, use a different font for the level names
+
+# colors:
+ACTIVE_COLOR = "#C00000"
+ACTIVE_LEVEL_COLOR = "#994444"
+BRACKET_COLOR = "#222222"
+REGULAR_COLOR = "#EEEEEE"
+
+
+def pretty_level(lev, indices, current):
+    if len(indices) == 1: # case of a single workspace level
+        if current[0] == lev:
+            return " <span weight='bold' color='{}'>{}</span> ".format(ACTIVE_COLOR, lev)
+        else:
+            return " <span weight='bold' color='{}'>{}</span> ".format(REGULAR_COLOR, lev)
+    else:
+        result = "<span color='{}'> [</span>".format(ACTIVE_COLOR if (lev == current[0]) else BRACKET_COLOR)
+        result += "<span style='italic' color='{}'>{}</span>".format(
+            ACTIVE_LEVEL_COLOR if (lev == current[0]) else REGULAR_COLOR,
+            lev
+        )
+        for index in sorted(indices, key=str.casefold):
+            result += "<span weight='bold' color='{}'> {} </span>".format(
+                ACTIVE_LEVEL_COLOR if (lev == current[0] and index == current[1]) else REGULAR_COLOR,
+                index
+            )        
+        result += "<span color='{}'>] </span>".format(ACTIVE_COLOR if (lev == current[0]) else BRACKET_COLOR)
+        return result
+        
+
+
     
 class Etabli:
     def __init__(self, wm):
@@ -17,8 +42,7 @@ class Etabli:
 
     def get_state_from_wm(self):
         self.levels = {}
-        self.current_level = None
-        self.current_index = None
+        self.current = [None, None]
         for sp in self.wm.get_workspaces():
             name = sp.name
             lev, index = split_workspace_name(name)
@@ -27,8 +51,7 @@ class Etabli:
             else:
                 self.levels[lev] = [index]
             if sp.focused:
-                self.current_level = lev
-                self.current_index = index
+                self.current = [lev, index]
                 
 
     def __str__(self):
@@ -37,28 +60,30 @@ class Etabli:
         bracket_close = "<span color='#111111'>]</span>"
         index_separator = "<span color='#555555'>|</span>"
         for lev in sorted(self.levels.keys(), key=str.casefold):
-            if lev == self.current_level:
-                pretty_lev = "<span color='#0000C0'>{}</span>".format(lev)
-            else:
-                pretty_lev = lev
-            if len(self.levels[lev]) == 1:
-                result += "{}{}{}".format(
-                    " <span weight='bold'>",
-                    pretty_lev,
-                    "</span> "
-                )
-            else:
-                result += bracket_open + "<span weight='bold'>{}</span>  ".format(
-                    pretty_lev,
-                )
-                for index in sorted(self.levels[lev], key=str.casefold):
-                    if lev == self.current_level and index == self.current_index:
-                        result += "<span color='#C00000' style='italic'>{}</span> ".format(index) + index_separator + " "
-                    else:
-                        result += "{} ".format(index) + index_separator + " "
-                result = result[:-(1+len(index_separator))] + bracket_close + " "
-        print("current: {}\n\n".format(self.current_level))
-        return result[:-1]
+            result += pretty_level(lev, self.levels[lev], self.current)
+        return result
+        #     if lev == self.current_level:
+        #         pretty_lev = "<span color='#0000C0'>{}</span>".format(lev)
+        #     else:
+        #         pretty_lev = lev
+        #     if len(self.levels[lev]) == 1:
+        #         result += "{}{}{}".format(
+        #             " <span weight='bold'>",
+        #             pretty_lev,
+        #             "</span> "
+        #         )
+        #     else:
+        #         result += bracket_open + "<span weight='bold'>{}</span>  ".format(
+        #             pretty_lev,
+        #         )
+        #         for index in sorted(self.levels[lev], key=str.casefold):
+        #             if lev == self.current_level and index == self.current_index:
+        #                 result += "<span color='#C00000' style='italic'>{}</span> ".format(index) + index_separator + " "
+        #             else:
+        #                 result += "{} ".format(index) + index_separator + " "
+        #         result = result[:-(1+len(index_separator))] + bracket_close + " "
+        # print("current: {}\n\n".format(self.current_level))
+        # return result[:-1]
 
 def waybar_input(SWAY, e):
     MAIN_ETABLI.get_state_from_wm()
