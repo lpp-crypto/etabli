39a95531e68f2e7bcf09633794f0b27a022ebbf3
Leo Perrin <leo.perrin@inria.fr>
2025-06-05 19:50:11 +0200

    automated renaming of workspaces

--- etabliConfig.py
+++ etabliConfig.py
@@ -31,7 +31,7 @@ class StartEmacsServer:
             LOG.info("Chose not to start emacs server {}".format(daemon_name))
 
 
-# !SECTION! The following table describes the mapping between level names and the functions that must be run when they are created
+# !SECTION! Describeing the mapping between level names and the functions that must be run when they are created
 
 launch_table = {
     "firefox" : [IfEmpty(),
@@ -56,10 +56,51 @@ launch_table = {
     ]
 }
 
+# !TODO! add something to "re-attach" loose emacs servers 
 
 
 
+# !SECTION!  Suggesting a name for a workspace within a level
+
+def rename_workspace_by_guessing():
+    focused = SWAY.get_tree().find_focused()
+    # finding potential name, modify this part to have other names!
+    new_name = "New"
+    if "emacs" in focused.app_id:
+        current_file_extension = focused.name.split(".")[-1]
+        if "tex" in current_file_extension:
+            new_name = "writing"
+        elif "org" in current_file_extension:
+            new_name = "org"
+        else:
+            new_name = "code"
+    elif "kitty" in focused.app_id:
+        new_name = "term"
+    elif "firefox" in focused.app_id:
+        if "youtube" in focused.name.lower():
+            new_name = "youtube"
+        else:
+            new_name = "firefox"
+    elif "evince" in focused.app_id:
+        new_name = "pdf"
+    elif "rhythmbox" in focused.app_id:
+        new_name = "music"
+    # ensuring that it is not already taken
+    current_level = current_level_name()
+    current_level_content = [x[1] for x in get_level(current_level)]
+    if new_name in current_level_content:
+        diversifier = 0
+        potential_new_name = new_name
+        while potential_new_name in current_level_content:
+            diversifier += 1
+            potential_new_name = new_name + "-{:d}".format(diversifier)
+        new_name = potential_new_name
+    SWAY.command("rename workspace to {}".format(
+        format_workspace_name(current_level, new_name)
+    ))
+
+
+# !SECTION! Testing
+    
 if __name__ == "__main__":
-    s = StartEmacsServer(daemon_name="*", additional_args=["--funcall=capture-theme"])
-    s()
-    LOG.info("bleeeh")
+    rename_workspace_by_guessing()
